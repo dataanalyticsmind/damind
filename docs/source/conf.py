dcf7a7421752f8adf506d63ba1288ce321512928
dataanalyticsmind <dataanalyticsmind@gmail.com>
2025-01-04 00:41:41 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'damind'
+copyright = '2025, DA Mind'
+author = 'admin'
 
 release = '0.1'
 version = '0.1.0'
